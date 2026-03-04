3a23ce9396c7e06e5851127cc91cc78c63eb27f7
Hera.r <tennyhera.r@gmail.com>
2026-03-03 19:43:48 -0500

    feat: add signal handling and ping statistics management
    - Implement SIGINT handling using sigaction
    - Add global stop flag for clean shutdown
    - Create stats structure to track sent/received packets
    - Implement packet loss calculation
    - Add RTT calculation (min/avg/max)
    - Print final statistics on Ctrl+C

--- main.c
+++ main.c
@@ -7,57 +7,96 @@
 #include <sys/socket.h>
 #include <sys/poll.h>
 #include <errno.h>
+#include <limits.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/time.h>
+#include <signal.h>
 
 #define BUFSIZE		64
-#define ICMP_ECHO	8
 
-/*	struct pour IPv4 
 
-	struct sockaddr_in {
-		sa_family_t		sin_family;
-		in_port_t		sin_port;
-		struct in_addr	sin_addr;
-	};
-
-	struct in_addr {
-		uint32_t	s_addr;
-	};
-
-*/
-
-// struct pollfd {
-//    int   fd;         /* file descriptor */
-//    short events;     /* Événements attendus */
-//    short revents;    /* Événements détectés */
-// };
+typedef struct s_stats
+{
+    int     sent;
+    int     received;
+    long    rtt_min;
+    long    rtt_max;
+    long    rtt_total;
+}   t_stats;
 
 
 
-// struct icmphdr
-// {
-//   u_int8_t type;                /* message type */
-//   u_int8_t code;                /* type sub-code */
-//   u_int16_t checksum;
-//   union
-//   {
-//     struct
-//     {
-//       u_int16_t        id;
-//       u_int16_t        sequence;
-//     } echo;                        /* echo datagram */
+volatile	sig_atomic_t g_stop = 0;
 
-//     u_int32_t        gateway;        /* gateway address */
+void	handle_sigint(int sig)
+{
+	(void)sig;
+	g_stop = 1;
+}
 
-//     struct
-//     {
-//       u_int16_t        __unused;
-//       u_int16_t        mtu;
-//     } frag;                        /* path mtu discovery */
-//   } un;
-// };
+void stats_packet_sent(t_stats *stats)
+{
+    stats->sent++;
+}
+
+void stats_packet_received(t_stats *stats)
+{
+    stats->received++;
+}
+
+double stats_packet_loss(t_stats *stats)
+{
+    if (stats->sent == 0)
+        return 0.0;
+
+    return ((stats->sent - stats->received) * 100.0) / stats->sent;
+}
+
+long calculate_rtt_ms(struct timeval *start, struct timeval *end)
+{
+    long sec = end->tv_sec - start->tv_sec;
+    long usec = end->tv_usec - start->tv_usec;
+
+    if (usec < 0)
+    {
+        sec--;
+        usec += 1000000;
+    }
+
+    return (sec * 1000) + (usec / 1000);
+}
+
+void stats_update_rtt(t_stats *stats, long rtt)
+{
+    stats->rtt_total += rtt;
+
+    if (rtt < stats->rtt_min)
+        stats->rtt_min = rtt;
+
+    if (rtt > stats->rtt_max)
+        stats->rtt_max = rtt;
+}
+
+
+double stats_rtt_avg(t_stats *stats)
+{
+    if (stats->received == 0)
+        return 0.0;
+
+    return (double)stats->rtt_total / stats->received;
+}
+
+
+void stats_init(t_stats *stats)
+{
+    stats->sent = 0;
+    stats->received = 0;
+    stats->rtt_total = 0;
+    stats->rtt_min = LONG_MAX;
+    stats->rtt_max = 0;
+}
+
 
 static unsigned short calcul_checksum(void *b, int len) {
 	unsigned short *buf = b;
@@ -94,7 +133,20 @@ u_int16_t	init_icmp_echo_request(struct icmphdr *icmp, char buf[], u_int16_t *se
 
 int	main()
 {
+    struct sigaction sa;
+    t_stats stats;
+
+	
+    sa.sa_handler = handle_sigint;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGINT, &sa, NULL);
+	
+	
+	
+	
 	struct sockaddr_in src, dst = {0};
+	struct in_addr addr;
 	int socket_fd;
 	char buf[BUFSIZE];
 	u_int16_t seq = 0;
@@ -103,22 +155,25 @@ int	main()
 	src.sin_addr.s_addr = INADDR_ANY;
 	
 	dst.sin_family = AF_INET;
-	inet_pton(AF_INET, "8.8.8.8", &dst.sin_addr);
+	inet_pton(AF_INET, "8.8.8.8", &dst.sin_addr); // gerer manuellement mais sera modifier
 	
 	socket_fd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
 	if (socket_fd == -1) {
 		fprintf(stderr, "socket error: %s\n", strerror(errno));
 		return 1;
 	}
-	
+
 	memset(buf, 0, BUFSIZE);
 	struct icmphdr *icmp = (struct icmphdr *)buf;
+	
+	stats_init(&stats);
 
 	printf("PING 8.8.8.8 (8.8.8.8): %lu data bytes\n",
-	       sizeof(struct icmphdr) + sizeof(struct timeval));
+	       sizeof(struct icmphdr) + sizeof(struct timeval)); // gerer manuellement mais sera modifier
 
-	while (1)
+	while (!g_stop)
 	{
+
 		u_int16_t sent_seq = init_icmp_echo_request(icmp, buf, &seq);
 
 		ssize_t send_ping = sendto(socket_fd, buf,
@@ -128,6 +183,8 @@ int	main()
 			fprintf(stderr, "send error: %s\n", strerror(errno));
 			return 1;
 		}
+
+		stats_packet_sent(&stats);
 
 		char rbuf[BUFSIZE];
 		socklen_t len_dst = sizeof(struct sockaddr_in);
@@ -141,10 +198,10 @@ int	main()
 
 		struct iphdr *ip = (struct iphdr *)rbuf;
 		struct icmphdr *icmp_reply = (struct icmphdr *)(rbuf + (ip->ihl * 4));
-		struct in_addr addr;
 		addr.s_addr = ip->saddr;
 
 		if (icmp_reply->type == ICMP_ECHOREPLY) {
+			stats_packet_received(&stats);
 
 			if ((ssize_t)recv_ping >= (ssize_t)(ip->ihl * 4 + sizeof(struct icmphdr) + sizeof(struct timeval))) {
 				struct timeval *tv_sent = (struct timeval *)(rbuf + (ip->ihl * 4) + sizeof(struct icmphdr));
@@ -153,6 +210,9 @@ int	main()
 
 				long rtt_ms = (tv_now.tv_sec - tv_sent->tv_sec) * 1000
 				              + (tv_now.tv_usec - tv_sent->tv_usec + 1000000) % 1000000 / 1000;
+
+				long rtt = calculate_rtt_ms(tv_sent, &tv_now);
+				stats_update_rtt(&stats, rtt);
 
 				printf("%zd bytes from %s icmp_seq=%d ttl=%d time=%ld ms\n",
 				       recv_ping, inet_ntoa(addr), sent_seq, ip->ttl, rtt_ms);
@@ -169,13 +229,17 @@ int	main()
 		sleep(1);
 	}
 
+	printf("\n--- %s ping statistics ---\n",inet_ntoa(addr));
+	printf("%d packets transmitted, %d received, %.0f%% packet loss\n",
+		stats.sent,
+		stats.received,
+		stats_packet_loss(&stats));
+
+	printf("rtt min/avg/max = %ld/%.2f/%ld ms\n",
+		stats.rtt_min,
+		stats_rtt_avg(&stats),
+		stats.rtt_max);
+
 	return 0;
 }
 
-// PING 8.8.8.8 (8.8.8.8) 56(84) bytes of data.
-// 64 bytes from 8.8.8.8: icmp_seq=1 ttl=116 time=5.80 ms
-// 64 bytes from 8.8.8.8: icmp_seq=2 ttl=116 time=6.93 ms
-// ^C
-// --- 8.8.8.8 ping statistics ---
-// 2 packets transmitted, 2 received, 0% packet loss, time 1001ms
-// rtt min/avg/max/mdev = 5.802/6.368/6.934/0.566 ms
